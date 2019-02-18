ad8fd4c1fd00f6b69751b3264f4d911fb340cb3a
Virgilio Neto <virgilio.missao.neto@gmail.com>
2019-02-18 09:47:21 -0300

    Added project files

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='simplemediator',
+    version='0.0.1',
+    description='Simple event based mediator engine',
+    author='Virgilio Neto',
+    author_email='virgilio.missao.neto@gmail.com',
+    python_requires='>=3.6',
+    classifiers=['Programming Language :: Python :: 3.6'],
+    packages=find_packages(),
+    url='https://github.com/virgilioneto/py-simple-mediator',
+    install_requires=[]
+)
